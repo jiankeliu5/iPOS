d19c3e07fc73b3a9b7d7b9fd549a5c2219213bf3
opi-tlomenda <opi-tlomenda@be13b793-0bd0-4dcc-8efa-9494b9a66bcc>
2011-02-04 22:17:49 +0000

    1)  Added ASIHTTPRequest Utility classes. 2)  Started to flush out the iPOSInterface method signatures.
    NOTE:  iPOSFacade will be responsible for tracking the session with the POS Web Services.  As part of this, the current device unique identifer will be retrieved and stored in a SessionInfo object.  This will get flushed out more as we go :-)

--- InventoryService.h
+++ InventoryService.h
@@ -5,13 +5,14 @@
 //  Created by Torey Lomenda on 2/2/11.
 //  Copyright 2011 Object Partners Inc. All rights reserved.
 //
+#import "SessionInfo.h"
 
 @protocol InventoryService <NSObject>
 
 #pragma mark Product Item Services
 @required
--(void) lookupProductItem;
--(BOOL) isProductItemAvailable;
+-(void) lookupProductItem: (NSString *) itemSku withSession:  (SessionInfo *) sessionInfo;
+-(BOOL) isProductItemAvailable:  (NSString *) itemId forQuantity: (NSDecimal *) quantity withSession:  (SessionInfo *) sessionInfo;
 
 
 @end
