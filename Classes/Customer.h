caef3fb6181d83fc8c4a5f17cb2cdaad1dd1da8a
opi-tlomenda <opi-tlomenda@be13b793-0bd0-4dcc-8efa-9494b9a66bcc>
2011-02-04 22:15:21 +0000

    1)  Added ASIHTTPRequest Utility classes. 2)  Started to flush out the iPOSInterface method signatures.
    NOTE:  iPOSFacade will be responsible for tracking the session with the POS Web Services.  As part of this, the current device unique identifer will be retrieved and stored in a SessionInfo object.  This will get flushed out more as we go :-)

---
+++ Classes/Customer.h
@@ -0,0 +1,16 @@
+//
+//  Customer.h
+//  iPOS
+//
+//  Created by Torey Lomenda on 2/4/11.
+//  Copyright 2011 Object Partners Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Customer : NSObject {
+
+}
+
+@end
---
+++ Classes/Customer.m
@@ -0,0 +1,14 @@
+//
+//  Customer.m
+//  iPOS
+//
+//  Created by Torey Lomenda on 2/4/11.
+//  Copyright 2011 Object Partners Inc. All rights reserved.
+//
+
+#import "Customer.h"
+
+
+@implementation Customer
+
+@end
