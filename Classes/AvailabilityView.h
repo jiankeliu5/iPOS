f673599f109c9cbea7cccd13cb880170252758b7
opi-smccoole <opi-smccoole@be13b793-0bd0-4dcc-8efa-9494b9a66bcc>
2011-03-03 03:52:32 +0000

    Clean up some unused defines in AddItemView that were left from splitting out the AvailabilityView.  Bump the font size from 11 to 14 in the AvailabilityView display.

--- Classes/AvailabilityView.h
+++ Classes/AvailabilityView.h
@@ -13,7 +13,7 @@
 #define AVAILABLE_COLOR [UIColor colorWithRed:170.0f/255.0f green:204.0f/255.0f blue:0.0f alpha:1.0f]
 #define UNAVAILABLE_COLOR [UIColor colorWithRed:255.0f/255.0f green:70.0f/255.0f blue:0.0f alpha:1.0f]
 #define LARGE_FONT_SIZE 16.0f
-#define SMALL_FONT_SIZE 11.0f
+#define SMALL_FONT_SIZE 14.0f
 #define BIG_LABEL_HEIGHT 16.0f
 #define SMALL_LABEL_HEIGHT 14.0f
 
