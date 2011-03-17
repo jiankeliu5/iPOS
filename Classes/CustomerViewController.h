4083ab823eb9d0918844f2e316a44d3289f008c5
opi-smccoole <opi-smccoole@be13b793-0bd0-4dcc-8efa-9494b9a66bcc>
2011-03-17 03:42:26 +0000

    Update CustomerViewController to have customer view layout since it triggers navigation.

--- Classes/CustomerViewController.h
+++ Classes/CustomerViewController.h
@@ -6,14 +6,51 @@
 //  Copyright 2011 NA. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "iPOSFacade.h"
-#import "CustomerView.h"
+#import "ExtUITextField.h"
+#import "MOGlassButton.h"
+
+#define START_Y 40.0f
+#define SPACING 20.0f
+#define TEXT_FIELD_HEIGHT 30.0f
+#define TEXT_FIELD_WIDTH 200.0f
+#define BUTTON_HEIGHT 30.0f
+#define BUTTON_WIDTH 100.0f
+#define LABEL_FONT_SIZE 12.0f
+#define LABEL_HEIGHT 12.0f
+#define LABEL_SPACING 7.0f
+#define DETAIL_VIEW_X 10.0f
+#define DETAIL_VIEW_WIDTH 300.0f
+#define DETAIL_VIEW_HEIGHT 77.0f
+#define DETAIL_LABEL_X 0.0f
+#define DETAIL_LABEL_WIDTH 80.0f
+#define DETAIL_DATA_X 90.0f
+#define DETAIL_DATA_WIDTH 210.0f
+#define CONFIRM_BUTTON_X 180.0f
 
 @interface CustomerViewController : UIViewController {
 	
 	iPOSFacade *facade;
-	CustomerView *custView;
+
+	ExtUITextField *custPhoneField;
+	MOGlassButton *custSearchButton;
+	MOGlassButton *custNewButton;
+	MOGlassButton *custEditButton;
+	MOGlassButton *confirmButton;
+	
+	UIView *detailView;
+	UILabel *firstLabel;
+	UILabel *firstName;
+	UILabel *lastLabel;
+	UILabel *lastName;
+	UILabel *emailLabel;
+	UILabel *email;
+	UILabel *zipLabel;
+	UILabel *zip;
+	
+	BOOL custDetailsOpen;
 	
 }
 
--- Classes/CustomerViewController.m
+++ Classes/CustomerViewController.m
@@ -8,11 +8,13 @@
 
 #import "CustomerViewController.h"
 #import "UIViewController+ViewControllerLayout.h"
+#import "UIView+ViewLayout.h"
 
 @interface CustomerViewController()
-
 - (void) handleSearchButton:(id)sender;
-
+- (UILabel *) createNormalLabel:(NSString *)text withRect:(CGRect)rect;
+- (UILabel *) createBoldLabel:(NSString *)text withRect:(CGRect)rect;
+- (void) updateViewLayout;
 @end
 
 @implementation CustomerViewController
@@ -53,14 +55,90 @@
 // Implement loadView to create a view hierarchy programmatically, without using a nib.
 - (void)loadView {
 
-	custView = [[CustomerView alloc] initWithFrame:[self rectForNav]];
+	UIView *custView = [[UIView alloc] initWithFrame:CGRectZero];
+	custView.backgroundColor = [UIColor colorWithWhite:0.85f alpha:1.0f];
 	[self setView:custView];
 	[custView release];
+	
+	custPhoneField = [[ExtUITextField alloc] initWithFrame:CGRectMake(0.0f, 0.0f, TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT)];
+	custPhoneField.textColor = [UIColor blackColor];
+	custPhoneField.borderStyle = UITextBorderStyleRoundedRect;
+	custPhoneField.textAlignment = UITextAlignmentCenter;
+	custPhoneField.clearsOnBeginEditing = YES;
+	custPhoneField.placeholder = @"Phone Number";
+	custPhoneField.tagName = @"CustPhone";
+	custPhoneField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+	custPhoneField.returnKeyType = UIReturnKeyGo;
+	custPhoneField.keyboardType = UIKeyboardTypeNumberPad;
+	[self.view addSubview:custPhoneField];
+	[custPhoneField release];
+	
+	custSearchButton = [[MOGlassButton alloc] initWithFrame:CGRectMake(0.0f, 0.0f, BUTTON_WIDTH, BUTTON_HEIGHT)];
+	[custSearchButton setupAsSmallBlackButton];
+	custSearchButton.titleLabel.textAlignment = UITextAlignmentCenter;
+	[custSearchButton setTitle:@"Search" forState:UIControlStateNormal];
+	[self.view addSubview:custSearchButton];
+	[custSearchButton release];
+	
+	// Set up the detail view for showing customer summary information when fetched by the search.
+	detailView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, DETAIL_VIEW_WIDTH, DETAIL_VIEW_HEIGHT)];
+	
+	CGFloat dy = LABEL_SPACING;
+	firstLabel = [self createNormalLabel:@"First Name" withRect:CGRectMake(DETAIL_LABEL_X, dy, DETAIL_LABEL_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:firstLabel];
+	[firstLabel release];
+	firstName = [self createBoldLabel:@"Megan" withRect:CGRectMake(DETAIL_DATA_X, dy, DETAIL_DATA_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:firstName];
+	[firstName release];
+	
+	dy += LABEL_HEIGHT + LABEL_SPACING;
+	lastLabel = [self createNormalLabel:@"Last Name" withRect:CGRectMake(DETAIL_LABEL_X, dy, DETAIL_LABEL_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:lastLabel];
+	[lastLabel release];
+	lastName = [self createBoldLabel:@"Hoy" withRect:CGRectMake(DETAIL_DATA_X, dy, DETAIL_DATA_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:lastName];
+	[lastName release];
+	
+	dy += LABEL_HEIGHT + LABEL_SPACING;
+	emailLabel = [self createNormalLabel:@"Email Address" withRect:CGRectMake(DETAIL_LABEL_X, dy, DETAIL_LABEL_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:emailLabel];
+	[emailLabel release];
+	email = [self createBoldLabel:@"Mhoy@tileshop.com" withRect:CGRectMake(DETAIL_DATA_X, dy, DETAIL_DATA_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:email];
+	[email release];
+	
+	dy += LABEL_HEIGHT + LABEL_SPACING;
+	zipLabel = [self createNormalLabel:@"Zip Code" withRect:CGRectMake(DETAIL_LABEL_X, dy, DETAIL_LABEL_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:zipLabel];
+	[zipLabel release];
+	zip = [self createBoldLabel:@"55441" withRect:CGRectMake(DETAIL_DATA_X, dy, DETAIL_DATA_WIDTH, LABEL_HEIGHT)];
+	[detailView addSubview:zip];
+	[zip release];
+	
+	detailView.hidden = YES;
+	[self.view addSubview:detailView];
+	[detailView release];
+	
+	custNewButton = [[MOGlassButton alloc] initWithFrame:CGRectMake(0.0f, 0.0f, BUTTON_WIDTH, BUTTON_HEIGHT)];
+	[custNewButton setupAsSmallBlackButton];
+	custNewButton.titleLabel.textAlignment = UITextAlignmentCenter;
+	[custNewButton setTitle:@"Enter New" forState:UIControlStateNormal];
+	custNewButton.hidden = YES;
+	[self.view addSubview:custNewButton];
+	[custNewButton release];
+	
+	custEditButton = [[MOGlassButton alloc] initWithFrame:CGRectMake(0.0f, 0.0f, BUTTON_WIDTH, BUTTON_HEIGHT)];
+	[custEditButton setupAsSmallBlackButton];
+	custEditButton.titleLabel.textAlignment = UITextAlignmentCenter;
+	[custEditButton setTitle:@"Edit" forState:UIControlStateNormal];
+	custEditButton.hidden = YES;
+	[self.view addSubview:custEditButton];
+	[custEditButton release];
 	
 }
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
+- (void) viewDidLoad {
     [super viewDidLoad];
 	
 	if (self.navigationController != nil) 
@@ -68,8 +146,41 @@
 		[self.navigationController setNavigationBarHidden:NO];
 	}
 	
-	[[custView custSearchButton] addTarget:self action:@selector(handleSearchButton:) forControlEvents:UIControlEventTouchUpInside];
+	[custSearchButton addTarget:self action:@selector(handleSearchButton:) forControlEvents:UIControlEventTouchUpInside];
 	
+}
+
+- (void) viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+	if (self.navigationController != nil) {
+		[self.navigationController setNavigationBarHidden:NO];
+	}
+	
+	[self updateViewLayout];
+	
+	// Do this last
+	[super viewWillAppear:animated];
+	
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+	// Call super at the beginning
+	[super viewDidAppear:animated];
+}
+
+- (void) viewWillDisappear:(BOOL)animated {
+	// Do this at the end
+	[super viewWillDisappear:animated];
+}
+
+- (void) viewDidDisappear:(BOOL)animated {
+	// Do this at the end
+	[super viewDidDisappear:animated];
 }
 
 // Override to allow orientations other than the default portrait orientation.
@@ -85,20 +196,62 @@
     // Release any cached data, images, etc. that aren't in use.
 }
 
-- (void)viewDidUnload {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
 #pragma mark -
 #pragma mark UIButton callbacks
 - (void)handleSearchButton:(id)sender {
 	NSLog(@"Got search button press");
-	if (custView.custDetailsOpen == NO) {
-		custView.custDetailsOpen = YES;
+	if (custDetailsOpen == NO) {
+		custDetailsOpen = YES;
 	} else {
-		custView.custDetailsOpen = NO;
+		custDetailsOpen = NO;
+	}
+	[self updateViewLayout];
+}
+
+#pragma mark -
+#pragma mark UILabel creation
+- (UILabel *) createNormalLabel:(NSString *)text withRect:(CGRect)rect {
+	UILabel *label;
+	label = [[UILabel alloc] initWithFrame:rect];
+	label.text = text;
+	label.backgroundColor = [UIColor clearColor];
+	label.textColor = [UIColor blackColor];
+	label.textAlignment = UITextAlignmentLeft;
+	label.font = [UIFont systemFontOfSize:LABEL_FONT_SIZE];
+	return label;
+}
+
+- (UILabel *) createBoldLabel:(NSString *)text withRect:(CGRect)rect {
+	UILabel *label = [self createNormalLabel:text withRect:rect];
+	label.font = [UIFont boldSystemFontOfSize:LABEL_FONT_SIZE];
+	return label;
+}
+
+#pragma mark -
+#pragma mark UIView update
+- (void) updateViewLayout {
+	CGFloat width = self.view.bounds.size.width;
+	
+	CGFloat cy = START_Y;
+	custPhoneField.center = [self.view centerAt:cy];
+	
+	if (custDetailsOpen == NO) {
+		cy += TEXT_FIELD_HEIGHT + SPACING;
+		detailView.hidden = YES;
+		custNewButton.hidden = YES;
+		custEditButton.hidden = YES;
+		custSearchButton.center = [self.view centerAt:cy];
+	} else {
+		cy += TEXT_FIELD_HEIGHT;
+		detailView.frame = CGRectMake(DETAIL_VIEW_X, cy, DETAIL_VIEW_WIDTH, DETAIL_VIEW_HEIGHT);
+		detailView.hidden = NO;
+		cy += DETAIL_VIEW_HEIGHT + SPACING;
+		CGFloat buttonSpace = floorf((width - BUTTON_WIDTH * 3.0f) / 4.0f);
+		custEditButton.frame = CGRectMake(buttonSpace, cy, BUTTON_WIDTH, BUTTON_HEIGHT);
+		custEditButton.hidden = NO;
+		custSearchButton.frame = CGRectMake(((buttonSpace * 2.0f) + BUTTON_WIDTH), cy, BUTTON_WIDTH, BUTTON_HEIGHT);
+		custNewButton.frame = CGRectMake((buttonSpace * 3.0f) + (BUTTON_WIDTH * 2.0f), cy, BUTTON_WIDTH, BUTTON_HEIGHT);
+		custNewButton.hidden = NO;
 	}
 }
 
