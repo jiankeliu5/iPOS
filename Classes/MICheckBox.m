9c867f5fbbc2a0bc2013eea60cfab8e1c7d35ade
opi-smccoole <opi-smccoole@be13b793-0bd0-4dcc-8efa-9494b9a66bcc>
2011-04-04 19:12:57 +0000

    Add checkbox for table multi-select.

---
+++ Classes/MICheckBox.m
@@ -0,0 +1,89 @@
+/*
+ 
+ Copyright (c) 2010, Mobisoft Infotech
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are
+ permitted provided that the following conditions are met:
+ 
+ Redistributions of source code must retain the above copyright notice, this list of
+ conditions and the following disclaimer.
+ 
+ Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ Neither the name of Mobisoft Infotech nor the names of its contributors may be used to
+ endorse or promote products derived from this software without specific prior written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ OF SUCH DAMAGE.
+ 
+ */
+
+
+#import "MICheckBox.h"
+
+@interface MICheckBox()
+@end
+
+@implementation MICheckBox
+
+@synthesize isChecked;
+@synthesize selectedImageName;
+@synthesize nonSelectedImageName;
+@synthesize tagName;
+
+- (id) initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+		
+		//self.frame =frame;
+		self.contentHorizontalAlignment  = UIControlContentHorizontalAlignmentLeft;
+		
+		// Default image names.  Need to be in the main application bundle!
+		self.selectedImageName = [NSString stringWithString:@"checkbox_ticked.png"];
+		self.nonSelectedImageName = [NSString stringWithString:@"checkbox_not_ticked.png"];
+
+		[self setImage:[UIImage imageNamed:self.nonSelectedImageName] forState:UIControlStateNormal];
+		[self addTarget:self action:@selector(checkBoxClicked) forControlEvents:UIControlEventTouchUpInside];
+	}
+    return self;
+}
+
+- (IBAction) checkBoxClicked {
+	if(self.isChecked == NO) {
+		self.isChecked = YES;
+		[self setImage:[UIImage imageNamed:self.selectedImageName] forState:UIControlStateNormal];
+		
+	} else {
+		self.isChecked = NO;
+		[self setImage:[UIImage imageNamed:self.nonSelectedImageName] forState:UIControlStateNormal];
+	}
+
+}
+
+- (void) dealloc {
+	[self setSelectedImageName:nil];
+	[self setNonSelectedImageName:nil];
+	[self setTagName:nil];
+    [super dealloc];
+}
+
+- (void) setupForRedCheckmark {
+	self.selectedImageName = [NSString stringWithString:@"IsSelectedRed.png"];
+	self.nonSelectedImageName = [NSString stringWithString:@"NotSelected.png"];
+	if (self.isChecked == NO) {
+		[self setImage:[UIImage imageNamed:self.nonSelectedImageName] forState:UIControlStateNormal];
+	} else {
+		[self setImage:[UIImage imageNamed:self.selectedImageName] forState:UIControlStateNormal];
+	}
+}
+
+@end
